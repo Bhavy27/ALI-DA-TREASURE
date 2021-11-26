08088e08f9a63cb1553e504bb0f36066743f570d
Bhavy27 <78476936+Bhavy27@users.noreply.github.com>
2021-11-26 21:53:12 +0530

    Add files via upload

---
+++ Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "RAW";
+const accessCode2 = "ALASKA";
+const accessCode3 = "MINECRAFT";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("Romeo Akbar Walter", 100,50)
+    fill("lightblue")
+    text("Hint: Indian Intelligence Agency", 100,70)
+
+    fill("white")
+    textSize(15)
+    text("Far From Home", 700,150)
+    fill("lightblue")
+    text("Hint: Part of USA but far from it", 700,170)
+
+    fill("white")
+    textSize(15)
+    text("M O J A N G", 100,250)
+    fill("lightblue")
+    text("Hint: this Game's company comes under Microsoft(enter game's name)", 100,270)
+
+}
